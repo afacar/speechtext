a1f1f729428edc99c33c05918c73483f35d1b359
unknown <acarahmetfaruk@gmail.com>
2019-09-11 15:43:00 +0300

    storage bucket is added to config too

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -15,7 +15,7 @@ var firebaseProdConfig = {
   authDomain: "speechtext-io.firebaseapp.com",
   databaseURL: "https://speechtext-io.firebaseio.com",
   projectId: "speechtext-io",
-  storageBucket: "",
+  storageBucket: "gs://speechtext-io.appspot.com/",
   messagingSenderId: "936987129989",
   appId: "1:936987129989:web:4dc13f4d3b3deeab4051ec"
 };
