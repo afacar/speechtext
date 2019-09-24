d7f4426523ffd7f329c21a4f06774bdc8275a588
unknown <acarahmetfaruk@gmail.com>
2019-09-24 19:51:34 +0300

    email verification alert is added to dashboard

--- src/containers/dashboard/dashboard.js
+++ src/containers/dashboard/dashboard.js
@@ -2,31 +2,58 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import _ from 'lodash';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Alert } from 'react-bootstrap';
 
 import { getFileList } from '../../actions';
 import '../../styles/dashboard.css';
 import UserHeader from '../user-header';
 import FileList from './file-list';
 import Transcription from './transcription';
+import Utils from '../../utils';
+const { auth } = Utils.firebase;
 
 class Dashboard extends Component {
+    state = {
+        emailVerified: null
+    }
+
     componentDidMount() {
-        if(_.isEmpty(this.props.user)) {
+        if (_.isEmpty(this.props.user)) {
             this.props.history.push('/');
         } else {
+            const { emailVerified } = auth().currentUser;
+            this.setState({ emailVerified })
             this.props.getFileList();
         }
     }
 
+    resendVerificationEmail = async () => {
+        let { currentUser } = auth();
+        await currentUser.sendEmailVerification()
+            .then(() => {
+                this.setState({ isSent: true })
+            })
+    }
+
     render() {
+        const { user } = this.props;
         return (
             <div>
                 <UserHeader />
                 <Container className='dashboard-container'>
+                    {!this.state.emailVerified && 
+                        <Alert variant='warning'>
+                            {'You need to'} <Alert.Link onClick={this.resendVerificationEmail}>Verify</Alert.Link> {`your email ${user.email}!`}
+                        </Alert>
+                    }
+                    {!this.state.emailVerified && this.state.isSent &&
+                        <Alert variant='warning'>
+                            {`A verification email sent to ${user.email}. Refresh page after verifying your mail.`}
+                        </Alert>
+                    }
                     <Row>
                         <Col lg="4" md="4">
-                            <FileList />
+                            <FileList emailVerified={this.state.emailVerified} />
                         </Col>
                         <Col lg="8" md="8">
                             <Transcription />
