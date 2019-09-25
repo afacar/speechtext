23a59bf72cd98d7b69b3c70eb55ce7d67335894c
unknown <acarahmetfaruk@gmail.com>
2019-09-25 12:15:56 +0300

    class attribute is converted to className

--- src/containers/dashboard/transcription.js
+++ src/containers/dashboard/transcription.js
@@ -256,9 +256,9 @@ class Transcription extends Component {
                 </div>
                 {
                     this.state.showSpinner &&
-                    <div class="d-flex justify-content-center mt-5">
-                        <div class="spinner-border" role="status">
-                            <span class="sr-only">Loading...</span>
+                    <div className="d-flex justify-content-center mt-5">
+                        <div className="spinner-border" role="status">
+                            <span className="sr-only">Loading...</span>
                         </div>
                     </div>
                 }
