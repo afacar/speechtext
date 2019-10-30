c4937ecfd7a1f989ae8cabe42d690b6ae148f508
unknown <acarahmetfaruk@gmail.com>
2019-10-30 14:04:43 +0300

    isNewUser problem is solved

--- src/actions/auth-actions.js
+++ src/actions/auth-actions.js
@@ -2,47 +2,36 @@ import _ from 'lodash';
 import Utils from '../utils';
 const { firestore } = Utils.firebase;
 
+const db = firestore();
 
 export const login = (data) => {
     return async (dispatch, getState) => {
-        const response = await firestore().collection('users').doc(data.uid).get()
-            .catch(error => {
-                // TODO: GET_USER_PROFILE_ERROR
-                console.log(error);
-            })
         const { uid } = data;
-        //_.isEmpty(response) || _.isEmpty(response.data())
-        if (data.isNewUser) {
-            const demoPlan = _.find(getState()['plans'], ['type', 'Demo']);
-            data = { ...data, currentPlan: demoPlan };
-            data.currentPlan.remainingMinutes = demoPlan.quota;
-            delete data.isNewUser;
-            await firestore().doc(`users/${uid}`).set(data)
-                .catch(error => {
-                    // TODO: SET_USER_PROFILE_ERROR
-                    console.log(error);
-                })
-        } else {
-            data = !_.isEmpty(response) && !_.isEmpty(response.data()) ? response.data() : data;
-        }
-        dispatch({
-            type: Utils.ActionTypes.LOGIN,
-            payload: data
-        });
-
-        firestore().collection('users').doc(uid).onSnapshot((snapshot) => {
-            var data = {};
+        db.doc(`users/${uid}`).onSnapshot(async (snapshot) => {
+            let userData = data;
             if (snapshot && snapshot.data && snapshot.data()) {
-                data = snapshot.data();
+                userData = snapshot.data();
+            } else if (data.isNewUser) {
+                const demoPlan = _.find(getState()['plans'], ['type', 'Demo']);
+                userData = { ...data, currentPlan: demoPlan };
+                userData.currentPlan.remainingMinutes = demoPlan.quota;
+                delete userData.isNewUser;
+    
+                await db.doc(`users/${uid}`).set(userData)
+                    .catch(error => {
+                        // TODO: SET_USER_PROFILE_ERROR
+                        console.log(error);
+                    })
             }
             dispatch({
                 type: Utils.ActionTypes.LOGIN,
-                payload: data
+                payload: userData
             });
         }, (error) => {
             // TODO: GET_USER_PROFILE_ERROR
             console.log(error)
         });
+
     }
 }
 
