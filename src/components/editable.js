300382857f74299f08f99aca2817a8de8528bb05
salix <sal.senturk@gmail.com>
2019-11-01 00:33:01 +0300

    editor enhancements

---
+++ src/components/editable.js
@@ -0,0 +1,95 @@
+import React, { PureComponent, useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const exclusiveKeyCodes = [16, 17, 18, 20, 27, 93, 225, 144];
+const keyPressed = {};
+var lastKeyPressed = undefined;
+
+class Editable extends PureComponent {
+
+    componentDidUpdate() {
+        if(this.props.isActive && document.activeElement !== ReactDOM.findDOMNode(this.editableRef)) {
+            setTimeout(() => {
+                this.editableRef.focus();
+            }, 100);
+        }
+    }
+
+    getCaretPos() {
+        let _range = document.getSelection().getRangeAt(0);
+        let range = _range.cloneRange();
+        range.selectNodeContents(this.editableRef);
+        range.setEnd(_range.endContainer, _range.endOffset);
+        return range.toString().length;
+    }
+
+    onKeyDown = (e) => {
+        lastKeyPressed = e.keyCode;
+        keyPressed[e.keyCode] = true;
+        if(e.keyCode === 13) {  // Return
+            e.preventDefault();
+            e.stopPropagation();
+            this.props.splitData(this.getCaretPos(), this.props.word.word.length);
+        } else if(e.keyCode === 8) {    // Backspace
+            if(this.props.wordIndex === 0 && this.getCaretPos() === 0) {
+                e.preventDefault();
+                e.stopPropagation();
+                this.props.mergeData();
+            }
+        }
+    }
+
+    handleChange = (e) => {
+        console.log('editable');
+        const { keyCode } = e;
+        const { index, wordIndex, changeIndexes, handleWordChange } = this.props;
+        keyPressed[keyCode] = false;
+        if(exclusiveKeyCodes.includes(keyCode)) return;
+        if(keyCode === 9) { // TAB
+            changeIndexes(keyPressed[16] ? index - 1 : index + 1, 0);
+        } else {
+            let value = document.getSelection().baseNode.data;
+            handleWordChange(index, wordIndex, value.trim());
+        }
+    }
+
+    onBlur = () => {
+        console.log('onBlur')
+        if(lastKeyPressed === 37 || lastKeyPressed === 39) {
+            let { index, wordIndex, changeIndexes } = this.props;
+            if(lastKeyPressed === 37) {
+                if(wordIndex !== 0) {
+                    wordIndex -= 1;
+                }
+            } else if(lastKeyPressed === 39) {
+                wordIndex += 1;
+            }
+            changeIndexes(index, wordIndex);
+        }
+    }
+
+    render = () => {
+        const { index, wordIndex, isActive, word, changeIndexes } = this.props;
+        return (
+            <span contentEditable='false' className='editable-content-wrapper'>
+                <span
+                    id={ 'editable-content-' + index + "_" + wordIndex }
+                    contentEditable='true'
+                    spellCheck='false'
+                    tabIndex={ index + wordIndex }
+                    ref={(input) => { this.editableRef = input; }} 
+                    onClick={ e => changeIndexes(index, wordIndex) }
+                    onKeyUp={ this.handleChange }
+                    onKeyDown={ this.onKeyDown }
+                    onFocus={ e => changeIndexes(index, wordIndex) }
+                    onBlur={ this.onBlur }
+                    className={ 'editable-content ' + (isActive ? 'active-word' : '') }
+                >
+                    { word.word + ' ' }
+                </span>
+            </span>
+        )
+    }
+}
+
+export default Editable;
--- src/components/speech-text-editor.js
+++ src/components/speech-text-editor.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
+import Editable from './editable';
 import '../styles/editor.css';
 
 const exclusiveKeyCodes = [16, 17, 18, 20, 27, 93, 225, 144];
@@ -61,9 +62,7 @@ class SpeechTextEditor extends Component {
 
     componentDidUpdate() {
         const { caretPosition, activeIndex, activeWordIndex } = this.state;
-        console.log('caret pos: ' + caretPosition + '-' + activeIndex + '-' + activeWordIndex);
         if(activeIndex > -1 && activeWordIndex > -1 && caretPosition > -1) {
-            console.log('caret pos 2: ' + caretPosition);
             if(this.refs['span_' + activeIndex + '_' + activeWordIndex]) {
                 let el = this.refs['span_' + activeIndex + '_' + activeWordIndex];
                 let value = el.innerText;
@@ -92,7 +91,6 @@ class SpeechTextEditor extends Component {
     }
 
     placeCaretAtEnd = (el) => {
-        // el.focus();
         if (typeof window.getSelection != "undefined"
                 && typeof document.createRange != "undefined") {
             var range = document.createRange();
@@ -129,52 +127,150 @@ class SpeechTextEditor extends Component {
     }
     
     keyUp = (e) => {
+        console.log('editor');
         let keyCode = e.keyCode;
-        keyPressed[keyCode] = false;
-        if(exclusiveKeyCodes.includes(keyCode)) return;
-        const { activeIndex, activeWordIndex } = this.state;
-        if(keyCode === 9) { // TAB
-            this.setState({
-                activeIndex: keyPressed[16] ? activeIndex - 1 : activeIndex + 1,
-                activeWordIndex: 0,
-                caretPosition: 0
-            })
+        if(keyCode === 13) {
+            e.preventDefault();
+            e.stopPropagation();
         }
-        if(keyCode === 13 || keyCode === 37 || keyCode === 38 || keyCode === 39 || keyCode === 40) { // 13 -> Enter, 37 -> ArrowLeft, 38 -> ArrawUp, 39 -> ArrowRight, 40 -> ArrowDown
-            var selectedObj = window.getSelection();
-            // var cursorPosition = selectedObj.getRangeAt(0) === 1 ? 'start' : 'end';
-            var childNodes = selectedObj.anchorNode.parentNode.childNodes;
+        // keyPressed[keyCode] = false;
+        // if(exclusiveKeyCodes.includes(keyCode)) return;
+        // const { activeIndex, activeWordIndex } = this.state;
+        // if(keyCode === 9) { // TAB
+        //     this.setState({
+        //         activeIndex: keyPressed[16] ? activeIndex - 1 : activeIndex + 1,
+        //         activeWordIndex: 0,
+        //         caretPosition: 0
+        //     })
+        // }
+        // if(keyCode === 13 || keyCode === 37 || keyCode === 38 || keyCode === 39 || keyCode === 40) { // 13 -> Enter, 37 -> ArrowLeft, 38 -> ArrawUp, 39 -> ArrowRight, 40 -> ArrowDown
+        //     var selectedObj = window.getSelection();
+        //     // var cursorPosition = selectedObj.getRangeAt(0) === 1 ? 'start' : 'end';
+        //     var childNodes = selectedObj.anchorNode.parentNode.childNodes;
 
-            let nodeId = childNodes[0].parentElement.id;
-            if(nodeId.startsWith('editable-content-')) {
-                let indexArr = nodeId.split('editable-content-')[1].split('_');
-                let currIndex = parseInt(indexArr[0]);
-                let currWordIndex = parseInt(indexArr[1]);
-                if(activeIndex !== currIndex || activeWordIndex !== currWordIndex) {
-                    let currValue = this.refs['span_' + currIndex + '_' + currWordIndex].innerText;
-                    let caretPosition = currValue.startsWith(' ') ? 1 : 0;
-                    if(currWordIndex < activeWordIndex || keyCode === 40) {
-                        caretPosition = currValue.length;
-                    }
-                    this.setState({
-                        activeIndex: currIndex,
-                        activeWordIndex: currWordIndex,
-                        caretPosition
-                    });
-                }
-            }
-            // 0_1
-            console.log(childNodes[0].parentElement.id);
-        } else {
-            if(this.refs['span_' + activeIndex + '_' + activeWordIndex]) {
-                let el = this.refs['span_' + activeIndex + '_' + activeWordIndex];
-                let value = el.innerText;
-                this.setState({
-                    caretPosition: this.getCaretPos(el)
-                });
-                this.props.handleWordChange(activeIndex, activeWordIndex, value.trim());
-            }
+        //     let nodeId = childNodes[0].parentElement.id;
+        //     if(nodeId.startsWith('editable-content-')) {
+        //         let indexArr = nodeId.split('editable-content-')[1].split('_');
+        //         let currIndex = parseInt(indexArr[0]);
+        //         let currWordIndex = parseInt(indexArr[1]);
+        //         if(activeIndex !== currIndex || activeWordIndex !== currWordIndex) {
+        //             let currValue = this.refs['span_' + currIndex + '_' + currWordIndex].innerText;
+        //             let caretPosition = currValue.startsWith(' ') ? 1 : 0;
+        //             if(currWordIndex < activeWordIndex || keyCode === 40) {
+        //                 caretPosition = currValue.length;
+        //             }
+        //             this.setState({
+        //                 activeIndex: currIndex,
+        //                 activeWordIndex: currWordIndex,
+        //                 caretPosition
+        //             });
+        //         }
+        //     }
+        //     // 0_1
+        //     console.log(childNodes[0].parentElement.id);
+        // } else {
+        //     if(this.refs['span_' + activeIndex + '_' + activeWordIndex]) {
+        //         let el = this.refs['span_' + activeIndex + '_' + activeWordIndex];
+        //         let value = el.innerText;
+        //         this.setState({
+        //             caretPosition: this.getCaretPos(el)
+        //         });
+        //         this.props.handleWordChange(activeIndex, activeWordIndex, value.trim());
+        //     }
+        // }
+    }
+
+    getTranscriptionText = (words) => {
+        let transcriptionText = '';
+        _.each(words, (word, index) => {
+            transcriptionText += (index > 0 ? ' ' : '') + word.word;
+        });
+        return transcriptionText;
+    }
+
+    splitData = (caretPos, wordLength) => {
+        let { editorData } = this.props;
+        const { activeIndex, activeWordIndex } = this.state;
+        
+        let wordIndex = activeWordIndex;
+        if(caretPos < wordLength / 2) {
+            wordIndex -= 1;
         }
+
+        let firstSplittedData = editorData[activeIndex];
+        let secondSplittedData = _.cloneDeep(firstSplittedData);
+
+        let endWord = firstSplittedData.alternatives[0].words[wordIndex];
+        firstSplittedData.alternatives[0].endTime = endWord.endTime;
+        firstSplittedData.alternatives[0].words.splice(wordIndex + 1);
+        firstSplittedData.alternatives[0].transcript = this.getTranscriptionText(firstSplittedData.alternatives[0].words);
+
+        let startWord = secondSplittedData.alternatives[0].words[wordIndex + 1];
+        secondSplittedData.alternatives[0].startTime = startWord.startTime;
+        secondSplittedData.alternatives[0].words.splice(0, wordIndex + 1);
+        secondSplittedData.alternatives[0].transcript = this.getTranscriptionText(secondSplittedData.alternatives[0].words);
+
+        editorData[activeIndex] = firstSplittedData;
+        editorData.splice(activeIndex + 1, 0, secondSplittedData);
+
+        this.setState({
+            editorData,
+            activeIndex: activeIndex + 1,
+            activeWordIndex: 0,
+            caretPosition: 0
+        });
+    }
+
+    mergeData = () => {
+        let { editorData } = this.props;
+        const { activeIndex } = this.state;
+        
+        if(activeIndex === 0) return;
+
+        let prevData = editorData[activeIndex - 1];
+        let currentData = editorData[activeIndex];
+        let prevWordLength = prevData.alternatives[0].words.length;
+
+        prevData.alternatives[0].words = prevData.alternatives[0].words.concat(currentData.alternatives[0].words);
+        let wordLength = prevData.alternatives[0].words.length;
+        prevData.alternatives[0].endTime = prevData.alternatives[0].words[wordLength - 1].endTime;
+        prevData.alternatives[0].transcript = this.getTranscriptionText(prevData.alternatives[0].words);
+
+        editorData[activeIndex - 1] = prevData;
+        editorData.splice(activeIndex, 1);
+
+        this.setState({
+            editorData,
+            activeIndex: activeIndex - 1,
+            activeWordIndex: prevWordLength,
+            caretPosition: 0
+        });
+    }
+
+    addZero = (value, length) => {
+        if(value < 10) return length === 3 ? '00' : '0' + value;
+        return value;
+    }
+
+    formatTime = ({ seconds, nanos }) => {
+        let formattedTime = '';
+        if(seconds > 60) {
+            let minutes = parseInt(seconds / 60);
+            seconds = seconds % 60;
+            if(minutes > 60) {
+                let hours = parseInt(minutes / 60);
+                minutes = minutes % 60;
+                formattedTime = this.addZero(hours) + ':';
+            } else {
+                formattedTime = '00:';
+            }
+            formattedTime += this.addZero(minutes) + ':';
+        } else {
+            formattedTime += '00:';
+        }
+        formattedTime += this.addZero(seconds) + '.' + this.addZero(nanos, 3);
+
+        return formattedTime;
     }
 
     render() {
@@ -187,19 +283,18 @@ class SpeechTextEditor extends Component {
     
                 let children = "";
                 children = _.map(alternative.words, (word, wordIndex) => {
+                    let isActive = activeIndex === index && activeWordIndex === wordIndex;
                     return (
-                        <span
-                            id={ 'editable-content-' + index + "_" + wordIndex }
-                            contentEditable='true'
-                            spellCheck='false'
-                            tabIndex={ index + wordIndex }
-                            // onFocus={ e => this.changeIndexes(index, wordIndex) }
-                            onClick={ e => this.changeIndexes(index, wordIndex) }
-                            className={ activeIndex === index && activeWordIndex === wordIndex ? 'active-word' : ''}
-                            ref={ 'span_' + index + '_' + wordIndex }
-                        >
-                            { (wordIndex > 0 ? ' ' : '') + word.word }
-                        </span>
+                            <Editable
+                                index={ index }
+                                wordIndex={ wordIndex }
+                                word={ word }
+                                changeIndexes={ this.changeIndexes }
+                                handleWordChange={ this.props.handleWordChange }
+                                isActive={ isActive }
+                                splitData={ this.splitData }
+                                mergeData= { this.mergeData }
+                            />
                     )
                 });
                 return (
@@ -210,14 +305,12 @@ class SpeechTextEditor extends Component {
                             contentEditable='false'
                             disabled={ true }
                         >
-                            { alternative.startTime.seconds + ' - ' + alternative.endTime.seconds }
+                            { this.formatTime(alternative.startTime) + ' - ' + this.formatTime(alternative.endTime) }
                         </div>
                         <div
                             id={ 'editable-content-' + index }
                             contentEditable='true'
                             disabled={ false }
-                            onKeyUp={ this.keyUp }
-                            onKeyDown={ e => keyPressed[e.keyCode] = true }
                             ref={ 'paragraph_' + index }
                         >
                             { children }
