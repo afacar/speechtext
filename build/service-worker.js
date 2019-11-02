f29726a2cbdde4023ebc2d6027e9a8e6f44aec78
salix <sal.senturk@gmail.com>
2019-11-02 19:00:15 +0300

    dev merge & build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.106f49a9483f651673a69746417f4387.js"
+  "/precache-manifest.225cfb45bc3792f015cdee14e8e5254a.js"
 );
 
 self.addEventListener('message', (event) => {
