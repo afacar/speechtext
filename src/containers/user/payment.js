079b9d5bc9251d963b76c1925ad895b107aa8c4f
unknown <acarahmetfaruk@gmail.com>
2019-11-05 14:19:44 +0300

    frontend is changed according to minutes rollover

--- src/containers/user/payment.js
+++ src/containers/user/payment.js
@@ -388,9 +388,14 @@ class Payment extends Component {
 
     changeCurrentPlan = () => {
         const { user } = this.props;
-        if (user.currentPlan.type !== 'Demo') {
+        if (user.currentPlan.type === 'Monthly') {
             this.setState({
                 showApprovement: true
+            });
+        } else if (user.currentPlan.type === 'PayAsYouGo') {
+            this.setState({
+                showApprovement: true,
+                confirmationBody2: true
             });
         } else {
             this.submitPlanChange();
@@ -408,6 +413,8 @@ class Payment extends Component {
         }).then(({ data }) => {
             if (data.success) {
                 Alert.success(intl.formatMessage({ id: 'Plan.Change.succesMessage' }));
+            } else {
+                Alert.error('Some error occured! Please try again later.')
             }
         })
             .catch(error => {
@@ -415,13 +422,15 @@ class Payment extends Component {
                 console.log(error);
             })
         this.setState({
-            showApprovement: false
+            showApprovement: false,
+            confirmationBody2: false
         });
     }
 
     cancelPlanChange = () => {
         this.setState({
-            showApprovement: false
+            showApprovement: false,
+            confirmationBody2: false
         })
     }
 
@@ -527,7 +536,7 @@ class Payment extends Component {
                             id: 'Plan.Change.confirmationTitle'
                         }}
                         bodyText={{
-                            id: 'Plan.Change.confirmationBody'
+                            id: this.state.confirmationBody2 ? 'Plan.Change.confirmationBody2' : 'Plan.Change.confirmationBody'
                         }}
                         successButton={{
                             id: 'Plan.Change.confirm'
