7ebe383cc86650c9f418c1ab653b8caa66ca7e89
salix <sal.senturk@gmail.com>
2019-11-10 12:50:57 +0300

    dev merge & build

---
+++ build/precache-manifest.d27595a2495ae1cfafe6ccdbd8f7b7bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a2105a94f8e871bc0cd4c6f3087eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af598078221f92f398d2",
+    "url": "/static/css/2.93faa247.chunk.css"
+  },
+  {
+    "revision": "e51d6a020f4973129c79",
+    "url": "/static/css/main.a45f51b9.chunk.css"
+  },
+  {
+    "revision": "af598078221f92f398d2",
+    "url": "/static/js/2.b9de33d8.chunk.js"
+  },
+  {
+    "revision": "e51d6a020f4973129c79",
+    "url": "/static/js/main.05317ff9.chunk.js"
+  },
+  {
+    "revision": "119689f92b52f950b698",
+    "url": "/static/js/runtime~main.5287d67b.js"
+  },
+  {
+    "revision": "536159e514129e33041c6e8a2032d0fa",
+    "url": "/static/media/banner.536159e5.png"
+  },
+  {
+    "revision": "e4c6e26ea1a57b0efec530a278cd2192",
+    "url": "/static/media/logo-en.e4c6e26e.png"
+  },
+  {
+    "revision": "614c150f35d42aa835a93ee45e7fbcf4",
+    "url": "/static/media/logo-tr.614c150f.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32465db47782d0af1dfe8f8b2b3fbcca.js"
+  "/precache-manifest.d27595a2495ae1cfafe6ccdbd8f7b7bb.js"
 );
 
 self.addEventListener('message', (event) => {
