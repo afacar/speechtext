a6824994ed26575a9974f5ff86f59d6690a2d822
salix <sal.senturk@gmail.com>
2019-11-17 19:41:15 +0300

    unused import fix

--- src/containers/landing/entry.js
+++ src/containers/landing/entry.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
-import _ from 'lodash';
-import { Container, Carousel, Row, Col, Card, Button } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { Container, Carousel } from 'react-bootstrap';
 
-import Banner from '../../assets/banner.png';
 import Banner1 from '../../assets/banner-1.jpg';
 import Banner2 from '../../assets/banner-2.jpg';
 import Banner3 from '../../assets/banner-3.jpg';
@@ -29,69 +24,9 @@ class Entry extends Component {
     }
 
     render() {
-        const { user } = this.props;
         return (
             <div className='main-div' >
                 <Container align="center">
-                    {/* <Row>
-                        <Col lg="6" md="12" sm="12" xs="12">
-                            <img src={ Banner } title='Speech to Text Transcription' alt='Speech to text Automatic Transcription Editor Banner' className='banner-image' />
-                        </Col>
-                        <Col lg="6" md="12" sm="12" xs="12">
-                            <Row className='list-container-style'>
-                                <Col lg="12">
-                                    <Card className='list-container-card'>
-                                        <h4 className='title-style'>
-                                            <span className='title-name-style'>Speechtext.io</span>
-                                            <FormattedMessage id="Banner.subText" />
-                                        </h4>
-                                        <ul>
-                                            <li className="d-flex">
-                                                <span className='badge-style'>
-                                                    <FontAwesomeIcon icon={ faCheck } color='#28a745' />
-                                                </span>
-                                                <div className='list-text-style'>
-                                                    <FormattedMessage id="Banner.feature1" />
-                                                </div>
-                                            </li>
-                                            <li className="d-flex align-items-center">
-                                                <span className='badge-style'>
-                                                    <FontAwesomeIcon icon={ faCheck } color='#28a745' />
-                                                </span>
-                                                <div className='list-text-style'>
-                                                    <FormattedMessage id="Banner.feature2" />
-                                                </div>
-                                            </li>
-                                            <li className="d-flex align-items-center">
-                                                <span className='badge-style'>
-                                                    <FontAwesomeIcon icon={ faCheck } color='#28a745' />
-                                                </span>
-                                                <div className='list-text-style'>
-                                                    <FormattedMessage id="Banner.feature3" />
-                                                </div>
-                                            </li>
-                                            <li className="d-flex align-items-center">
-                                                <span className='badge-style'>
-                                                    <FontAwesomeIcon icon={ faCheck } color='#28a745' />
-                                                </span>
-                                                <div className='list-text-style'>
-                                                    <FormattedMessage id="Banner.feature4" />
-                                                </div>
-                                            </li>
-                                            {
-                                                _.isEmpty(user) &&
-                                                <li className="d-flex align-items-center">
-                                                    <Button variant='secondary' className='margin-left-25' onClick={ () => this.setState({ showAuth: true }) } >
-                                                        <FormattedMessage id="Banner.tryForFree" />
-                                                    </Button>
-                                                </li>
-                                            }
-                                        </ul>
-                                    </Card>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row> */}
                     <Carousel nextIcon={<span aria-hidden="true" className="carousel-control-next-icon" />}>
                         <Carousel.Item>
                             <img
