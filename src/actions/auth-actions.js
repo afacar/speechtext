f5c55898cb0f466a5ab64dfd59a0a0e97efaef22
afacar <acarahmetfaruk@gmail.com>
2019-12-10 17:40:27 +0300

    ip of new user is added to user profile

--- src/actions/auth-actions.js
+++ src/actions/auth-actions.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import Utils from '../utils';
+import publicIp from 'public-ip';
 const { firestore, auth } = Utils.firebase;
 
 const db = firestore();
@@ -9,14 +10,14 @@ export const login = (data) => {
         const { uid } = data;
         db.doc(`users/${uid}`).onSnapshot(async (snapshot) => {
             let userData = data;
+            let ip = await publicIp.v4();
             if (snapshot && snapshot.data && snapshot.data()) {
                 userData = snapshot.data();
             } else if (data.isNewUser) {
                 const demoPlan = _.find(getState()['plans'], ['type', 'Demo']);
-                userData = { ...data, currentPlan: demoPlan };
+                userData = { ...data, currentPlan: demoPlan, ip };
                 userData.currentPlan.remainingMinutes = demoPlan.quota;
                 delete userData.isNewUser;
-
                 await db.doc(`users/${uid}`).set(userData)
                     .then(res => {
                         !data.emailVerified && auth().currentUser.sendEmailVerification()
