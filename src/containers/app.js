96c4601487c2de84438765aaff9a9b066c47ef8b
afacar <acarahmetfaruk@gmail.com>
2019-12-17 15:51:32 +0300

    onauthStateChanged removed

--- src/containers/app.js
+++ src/containers/app.js
@@ -3,10 +3,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Alert from 'react-s-alert';
 
-import Utils from '../utils';
-import { bake_cookie } from 'sfcookies';
-import { withRouter } from 'react-router';
-
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
 
@@ -20,7 +16,6 @@ import ScrollTop from '../components/scroll-top';
 import FrequentlyAskedQuestions from './landing/faq';
 import Evaluate from './../components/evaluation-popup';
 
-const { firebase } = Utils;
 
 class App extends Component {
     componentDidMount() {
@@ -28,54 +23,27 @@ class App extends Component {
         this.props.setSupportedLanguages(this.props.supportedLanguages);
         this.props.getPlans();
         this.props.getErrorDefinitions(this.props.language);
-    
-        var that = this;
-        firebase.auth().onAuthStateChanged(user => {
-            const currentUser = user ? user : '';
-            that.setState({ user: currentUser });
-            if (currentUser) {
-                const { uid, displayName, email, emailVerified, metadata } = currentUser;
-                const { lastSignInTime, creationTime } = metadata;
-                const isNewUser = creationTime === lastSignInTime
-                let now = new Date()
-                let sinceLogin = now.getTime() - new Date(lastSignInTime).getTime() 
-                console.log('sinceLogin:', sinceLogin) 
-                const loginInfo = {
-                    uid,
-                    displayName,
-                    email,
-                    isNewUser,
-                    emailVerified,
-                    creationTime: new Date(creationTime),
-                };
-                that.props.login(loginInfo);
-
-                bake_cookie('speechtext-dev-login', loginInfo);
-                if(sinceLogin < 4500) {
-                    // Redirect new user to dashboard 
-                    this.props.history.push('/dashboard')
-                }
-            }
-        });
     }
 
     render() {
         return (
             <div>
+                <BrowserRouter>
                     <ScrollTop>
-                        <Route exact path='/' render={ props => <Main {...props} /> } />
-                        <Route path='/dashboard' component={ Dashboard } />
-                        <Route path='/user' component={ User } />
-                        <Route path='/privacy' component={ Privacy } />
-                        <Route path='/terms' component={ Terms } />
-                        <Route path='/faq' component={ FrequentlyAskedQuestions } />
-                        <Route path='/feedback/:fileInfo' component={ Evaluate } />
-                        <Route exact path='/feedback' component={ Evaluate } />
+                        <Route exact path='/' render={props => <Main {...props} />} />
+                        <Route path='/dashboard' component={Dashboard} />
+                        <Route path='/user' component={User} />
+                        <Route path='/privacy' component={Privacy} />
+                        <Route path='/terms' component={Terms} />
+                        <Route path='/faq' component={FrequentlyAskedQuestions} />
+                        <Route path='/feedback/:fileInfo' component={Evaluate} />
+                        <Route exact path='/feedback' component={Evaluate} />
                     </ScrollTop>
-                <Alert stack={{ limit: 3 }} timeout={ 5000 } html={ true } effect={ 'slide' } position={ 'top-right' } />
+                </BrowserRouter>
+                <Alert stack={{ limit: 3 }} timeout={5000} html={true} effect={'slide'} position={'top-right'} />
             </div>
         )
     }
 }
 
-export default connect(null, { setLanguage, setSupportedLanguages, getPlans, getErrorDefinitions, login })(withRouter(App));
+export default connect(null, { setLanguage, setSupportedLanguages, getPlans, getErrorDefinitions, login })(App);
