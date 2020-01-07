20b053f10a15fcb7f5295a2f696c1680cb37883d
salix <sal.senturk@gmail.com>
2020-01-07 20:16:26 +0300

    some mobile fixes

--- src/containers/landing/how-it-works.js
+++ src/containers/landing/how-it-works.js
@@ -18,8 +18,8 @@ const HowItWorks = (props) => {
                         <b><FormattedMessage id="HowItWorks.message" /></b>
                     </h5>
                     <Row className='steps-container'>
-                        <Col lg='2' md='2'></Col>
-                        <Col lg='4' md='4'>
+                        <Col lg='2' md='2' sm='0' xs='0'></Col>
+                        <Col lg='4' md='4' sm='12' xs='12'>
                             <Card className="h-100 text-center">
                                 <span className='span-style'>1</span>
                                 <FontAwesomeIcon 
@@ -33,12 +33,12 @@ const HowItWorks = (props) => {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col lg='2' md='2'className='steps-direction-right'></Col>
+                        <Col lg='2' md='2' sm='6' xs='6' className='steps-direction-right'></Col>
                     </Row>
                     <Row className='steps-container'>
-                        <Col lg='4' md='4'></Col>
-                        <Col lg='2' md='2'className='steps-direction-left'></Col>
-                        <Col lg="4" mb="4">
+                        <Col lg='4' md='4' className='d-none d-lg-block d-md-block'></Col>
+                        <Col lg='2' md='2' className='steps-direction-left d-none d-lg-block d-md-block'></Col>
+                        <Col lg="4" mb="4" sm='12' xs='12'>
                             <Card className="h-100 text-center">
                                 <span className='span-style'>2</span>
                                 <FontAwesomeIcon
@@ -52,10 +52,11 @@ const HowItWorks = (props) => {
                                 </Card.Body>
                             </Card>
                         </Col>
+                        <Col lg='0' md='0' sm='6' xs='6' className='steps-direction-right step-direction-mobile'></Col>
                     </Row>
                     <Row className='steps-container'>
-                        <Col lg='2' md='2'></Col>
-                        <Col lg="4" mb="4">
+                        <Col lg='2' md='2' sm='0' xs='0'></Col>
+                        <Col lg="4" mb="4" sm='12' xs='12'>
                             <Card className="h-100 text-center">
                                 <span className='span-style'>3</span>
                                 <FontAwesomeIcon
@@ -69,12 +70,12 @@ const HowItWorks = (props) => {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col lg='2' md='2'className='steps-direction-right'></Col>
+                        <Col lg='2' md='2' sm='6' xs='6' className='steps-direction-right'></Col>
                     </Row>
                     <Row className='steps-container'>
-                        <Col lg='4' md='4'></Col>
-                        <Col lg='2' md='2'className='steps-direction-left'></Col>
-                        <Col lg="4" mb="4">
+                        <Col lg='4' md='4' sm='0' xs='0'></Col>
+                        <Col lg='2' md='2' className='steps-direction-left d-none d-lg-block d-md-block'></Col>
+                        <Col lg="4" mb="4" sm='12' xs='12'>
                             <Card className="h-100 text-center">
                                 <span className='span-style'>4</span>
                                 <FontAwesomeIcon
@@ -88,17 +89,11 @@ const HowItWorks = (props) => {
                                 </Card.Body>
                             </Card>
                         </Col>
+                        <Col lg='0' md='0' sm='6' xs='6' className='steps-direction-right step-direction-mobile'></Col>
                     </Row>
                     <Row className='steps-container'>
-                        <Col lg='2' md='2'></Col>
-                        {/* <Col lg="4" mb="4">
-                            <Card className="h-100 text-center">
-                                <Card.Body>
-                                    <img src={ FileLogo } />
-                                </Card.Body>
-                            </Card>
-                        </Col> */}
-                        <Col lg="4" mb="4">
+                        <Col lg='2' md='2' sm='0' xs='0'></Col>
+                        <Col lg="4" mb="4" sm='12' xs='12'>
                             <Card className="h-100 text-center">
                                 <span className='span-style'>
                                     <FontAwesomeIcon
@@ -108,6 +103,8 @@ const HowItWorks = (props) => {
                                     icon={faFilePdf}
                                     title='Export Your Transcription'
                                     className='image-style' size="6x" />
+                                <Card.Body>
+                                </Card.Body>
                             </Card>
                         </Col>
                     </Row>
