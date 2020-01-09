f765b95a649e9870891f2212d090b8f9e94ab207
salix <sal.senturk@gmail.com>
2020-01-09 20:50:23 +0300

    pricing page adjustments

--- src/containers/landing/pricing.js
+++ src/containers/landing/pricing.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import _ from 'lodash';
-import { FormattedMessage } from 'react-intl';
-import { Container, Button } from 'react-bootstrap';
+import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import { Row, Col } from 'react-bootstrap';
 
 import '../../styles/pricing.css';
 import Auth from '../../components/auth';
@@ -58,60 +58,81 @@ class Pricing extends Component {
                     <p className='title'>
                         <FormattedMessage id="Pricing.title" />
                     </p>
-                    <h3 align='center'>
+                    <h3>
                         <i><FormattedMessage id='Pricing.message' /></i>
                     </h3>
                     <br />
-                    <svg version="1.2" class="graph" aria-labelledby="title" role="img">
-                        <polyline
-                            fill="none"
-                            stroke="#384d60"
-                            stroke-width="5"
-                            points="
-                            90,55
-                            392,139
-                            684,223
-                            850,265
-                            1000,265"/>
-                        <g class="grid x-grid" id="xGrid">
-                        <line x1="90" x2="90" y1="5" y2="371"></line>
-                        </g>
-                        <g class="grid y-grid" id="yGrid">
-                            <line x1="90" x2="1005" y1="370" y2="370"></line>
-                        </g>
-                        <g class="labels x-labels">
-                            <text x="100" y="400">1</text>
-                            <text x="246" y="400">10</text>
-                            <text x="392" y="400">20</text>
-                            <text x="538" y="400">30</text>
-                            <text x="684" y="400">40</text>
-                            <text x="850" y="400">50+</text>
-                            <text x="960" y="400">
-                                <FormattedMessage id='Pricing.contactUs' />
-                            </text>
-                            <text x="500" y="440" class="label-title">
-                                <FormattedMessage id='Pricing.hoursLabel' />
-                            </text>
-                        </g>
-                        <g class="labels y-labels">
-                            <text x="80" y="55">$9 <FormattedMessage id='Pricing.hourSubText' /></text>
-                            <text x="60" y="200" class="label-title">
-                                <FormattedMessage id='Pricing.priceLabel' />
-                            </text>
-                        </g>
-                        <g class="data" data-setname="Our first data set">
-                            <circle cx="246" cy="98" data-value="8.1" r="5"></circle>
-                            <text x='216' y='132'>$8 <FormattedMessage id='Pricing.hourSubText' /></text>
-                            <circle cx="392" cy="140" data-value="7.7" r="5"></circle>
-                            <text x='362' y='174'>$7 <FormattedMessage id='Pricing.hourSubText' /></text>
-                            <circle cx="538" cy="182" data-value="6.8" r="5"></circle>
-                            <text x='508' y='216'>$6 <FormattedMessage id='Pricing.hourSubText' /></text>
-                            <circle cx="684" cy="223" data-value="6.7" r="5"></circle>
-                            <text x='654' y='257'>$5 <FormattedMessage id='Pricing.hourSubText' /></text>
-                            <circle cx="850" cy="265" data-value="6.7" r="5"></circle>
-                            <text x='820' y='299'>$4.5 <FormattedMessage id='Pricing.hourSubText' /></text>
-                        </g>
-                    </svg>
+                    <Row>
+                        <Col lg='7' md='7'>
+                            <svg version="1.2" class="graph" aria-labelledby="title" role="img">
+                                <polyline
+                                    fill="none"
+                                    stroke="#384d60"
+                                    stroke-width="5"
+                                    points="
+                                    90,77
+                                    392,161
+                                    684,245
+                                    850,287
+                                    900,287"/>
+                                <g class="grid x-grid" id="xGrid">
+                                <line x1="90" x2="90" y1="5" y2="371"></line>
+                                </g>
+                                <g class="grid y-grid" id="yGrid">
+                                    <line x1="90" x2="905" y1="370" y2="370"></line>
+                                </g>
+                                <g class="labels x-labels">
+                                    <text x="100" y="400">1</text>
+                                    <text x="186" y="400">10</text>
+                                    <text x="326" y="400">20</text>
+                                    <text x="466" y="400">30</text>
+                                    <text x="606" y="400">40</text>
+                                    <text x="746" y="400">50+</text>
+                                    <text x="850" y="400">
+                                        <FormattedMessage id='Pricing.contactUs' />
+                                    </text>
+                                    <text x="450" y="440" class="label-title">
+                                        <FormattedMessage id='Pricing.hoursLabel' />
+                                    </text>
+                                </g>
+                                <g class="labels y-labels">
+                                    <text x="86" y="77">$9 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                    <text x="60" y="200" class="label-title">
+                                        <FormattedMessage id='Pricing.priceLabel' />
+                                    </text>
+                                </g>
+                                <g class="data" data-setname="Our first data set">
+                                    <circle cx="186" cy="104" data-value="8.1" r="5"></circle>
+                                    <text x='156' y='132'>$8 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                    <circle cx="326" cy="142" data-value="7.7" r="5"></circle>
+                                    <text x='296' y='174'>$7 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                    <circle cx="466" cy="182" data-value="6.8" r="5"></circle>
+                                    <text x='436' y='216'>$6 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                    <circle cx="606" cy="223" data-value="6.7" r="5"></circle>
+                                    <text x='574' y='257'>$5 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                    <circle cx="746" cy="260" data-value="6.7" r="5"></circle>
+                                    <text x='720' y='299'>$4.5 <FormattedMessage id='Pricing.hourSubText' /></text>
+                                </g>
+                            </svg>
+                        </Col>
+                        <Col lg='5' md='5' className='side-message'>
+                            <div className='side-message-title'>
+                                <FormattedHTMLMessage id='Pricing.sideMessage.title' />
+                            </div>
+                            <br />
+                            <ul>
+                                <li>
+                                    <FormattedMessage id='Pricing.sideMessage.message1' />
+                                </li>
+                                <li>
+                                    <FormattedMessage id='Pricing.sideMessage.message2' />
+                                </li>
+                                <li>
+                                    <FormattedMessage id='Pricing.sideMessage.message3' />
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
                 </div>
                 <div className='pricing-mobile-content'>
                     <p className='title'>
