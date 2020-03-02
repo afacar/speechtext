f567cf243f1995e176b81ad25eb304608db749a5
afacar <acarahmetfaruk@gmail.com>
2020-03-02 15:24:52 +0300

    empty words cases handled under transcript alternatives

--- src/utils/transcription/convertToJSON.js
+++ src/utils/transcription/convertToJSON.js
@@ -2,21 +2,25 @@
 const convertToJSON = (transcript) => {
   let newJson = { speakers: [], segments: [] }
 
-  if(transcript.segments) return transcript;
+  if (transcript.segments) return transcript;
+  console.log('transcript', transcript)
 
   for (let i = 0; i < transcript.length; i++) {
     var { alternatives } = transcript[i];
     var { words, confidence, startTime, endTime } = alternatives[0];
-    let speaker = words[0].speakerTag
-    let newWords = words.map((word, ind) => {
-      return {
-        start: parseFloat(word.startTime.seconds + '.' + word.startTime.nanos / 100),
-        end: parseFloat(word.endTime.seconds + '.' + word.endTime.nanos / 100),
-        text: word.word
-      }
-    })
-    newJson.speakers.push({ name: '' })
-    newJson.segments.push({ words: newWords, speaker })
+    if (words) {
+      let speaker = words[0].speakerTag
+      let newWords = words.map((word, ind) => {
+        return {
+          start: parseFloat(word.startTime.seconds + '.' + word.startTime.nanos / 100),
+          end: parseFloat(word.endTime.seconds + '.' + word.endTime.nanos / 100),
+          text: word.word
+        }
+      })
+      newJson.speakers.push({ name: '' })
+      newJson.segments.push({ words: newWords, speaker })
+
+    }
   }
 
   return newJson
